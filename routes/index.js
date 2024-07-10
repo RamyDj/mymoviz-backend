bd1074839392fc47002ccc25f54d6eb6aadb76bc
Ramy <ramyto.rd@gmail.com>
2024-07-10 14:15:19 +0200

    back

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+require('dotenv').config();
+const fetch = require('node-fetch');
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
+
+router.get('/movies', (req, res) => {
+    
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
+    .then(response => response.json())
+    .then(apiData => {
+        res.json({movies: apiData.results});
+    })
+})
+
+
+
+
+module.exports = router;
